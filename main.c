668990c244f5088d9653b23e67539cfcb110aa88
pharris.sean <pharris.sean@gmail.com>
2022-01-05 15:09:48 -0500

    Code is now functional

--- main.c
+++ main.c
@@ -1,32 +1,50 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-  
+#include <time.h>
+#include <unistd.h>
 
 void passwordGenerator(int desiredLength)
 {   
-    char numbers[] = "0123456789";
-  
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-    char capital[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-    char symbols[] = "!@#$^&*?/";
-  
+    
+    char characters[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@|#$%^&*()-{}[]:;\\<>./?~`";
+    
     char password[desiredLength];
-  
+
     int counter = 0;
+
+    srand(time(NULL));
+
+    printf("Your password is: \n");
+
+    printf("---------------------------------------------\n");
+
+    for(counter = 0 ; counter < desiredLength ; counter++ ) {
+        int index = rand() % 89;
+        password[counter] = characters[index];
+        printf("%c", password[counter]);
+    }
+    
+
+    printf("\n");
+    printf("---------------------------------------------\n");
+   
 }
   
-
   
 int main()
 {
     int desiredLength;
+
     printf("How many character do you want your password to be?\n");
+    printf("The password will be this long: ");
     scanf("%i", &desiredLength);
-    
+    printf("---------------------------------------------\n");
     passwordGenerator(desiredLength);
-  
+     
     return 0;
 }
+
+
+
+
+
