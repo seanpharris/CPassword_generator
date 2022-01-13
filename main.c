9d7ca71714d650e00fdb85c5396fe3045465d043
pharris.sean <pharris.sean@gmail.com>
2022-01-13 12:14:14 -0500

    Rearranged UI

--- main.c
+++ main.c
@@ -6,36 +6,40 @@
 /* 
 TO DO
 - Add specific modifiers such as:
-    - Min amount of specifc characters (i.e. Capitals, Numbers, Special char)
+    - Min amount of specific characters (i.e. Capitals, Numbers, Special char)
 - Save passwords with an association
 - Some sort of encryption for saved password
 - decrypt when viewing password and association
 */
 
+/*
+compile code - gcc -o main main.c
+run code - ./main
+*/
 void passwordGenerator(int desiredLength)
 {   
     
     char characters[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@|#$%^&*()-{}[]:;\\<>./?~`";
-    
+
+    char specChar[28] = {"!@|#$%^&*()-{}[]:;\\<>./?~`\""};
+
     char password[desiredLength];
+    
+    char symbols[desiredLength];
 
     int counter = 0;
 
     srand(time(NULL));
 
-    printf("Your password is: \n");
-
-    printf("---------------------------------------------\n");
-
     for(counter = 0 ; counter < desiredLength ; counter++ ) {
         int index = rand() % 89;
         password[counter] = characters[index];
         printf("%c", password[counter]);
+        symbols[counter] = specChar[counter];
     }
     
 
-    printf("\n");
-    printf("---------------------------------------------\n");
+    printf("\n%s", symbols);  
    
 }
   
@@ -45,10 +49,20 @@ int main()
     int desiredLength;
 
     printf("How many character do you want your password to be?\n");
-    printf("The password will be this long: ");
+
+    printf("Your password will be ");
+
     scanf("%i", &desiredLength);
+
+    printf("characters long.\n");
+
     printf("---------------------------------------------\n");
+
+    printf("Your password is: \n");
+
     passwordGenerator(desiredLength);
+
+    printf("\n---------------------------------------------\n");
      
     return 0;
 }
